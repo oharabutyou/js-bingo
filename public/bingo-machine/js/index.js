d0376cdb49234110c3ee18abecee127c8a315dfe
oharabutyou <oharabutyou@gmail.com>
2023-12-02 21:05:05 +0900

    音が鳴るようになった、一番上に追加するようにした

--- public/bingo-machine/js/index.js
+++ public/bingo-machine/js/index.js
@@ -2,23 +2,26 @@ $(() => {
     $("#history-box").html("01.  1 B\n02.  2 B");
     $("#control-button").click(btn_click);
 });
-
+let sound = new Audio(
+    "https://raw.githubusercontent.com/Yousuke777/sound/main/kansei.mp3"
+);
 const btn_click = () => {
     const main_number = $("#main-number");
     main_number.html(parseInt(main_number.html()) + 1);
     push_number(1, 1);
+    sound.play();
 };
 
 const push_number = (num, index) => {
     const list_number = $("#history-box");
     list_number.html(
-        list_number.html() +
-            "\n" +
-            String(index).padStart(2, "0") +
+        String(index).padStart(2, "0") +
             ". " +
             String(num).padStart(2, " ") +
             " " +
-            select_char(num)
+            select_char(num) +
+            "\n" +
+            list_number.html()
     );
 };
 

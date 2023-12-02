cb1eb0df1dc8ba4242e64bab35f91a7e60ec616b
Ock Seungha <seungha.ock@mb.lecip.co.jp>
2023-12-02 20:11:04 +0900

    レイアウトヲツクッテマス

--- public/smartphone-client/js/logic-dayo.js
+++ public/smartphone-client/js/logic-dayo.js
@@ -1,4 +1,18 @@
+const BOARD_SIZE = 5;
 
+const $container = $(".container");
+const $row = $("<div class='row'></div>");
+const $col = $("<div class='col text-center'>c</div>");
+
+let usedNumArray = [];
+
+for (let r = 0; r < BOARD_SIZE; ++r) {
+    let $temp_row = $row.clone();
+    for (let c = 0; c < BOARD_SIZE; ++c) {
+        $temp_row.append($col.clone());
+    }
+    $container.append($temp_row);
+}
 
 
 
